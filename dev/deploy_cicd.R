4dfb9d0f0f7aec45c4772c586b5712d6e260b855
clement <clement.berthet013@gmail.com>
2025-02-15 23:02:30 +0100

    add ci/cd to deploy app on shinyapp.io

---
+++ dev/deploy_cicd.R
@@ -0,0 +1,18 @@
+# Deploy to Posit Connect or ShinyApps.io
+# In command line.
+rsconnect::deployApp(
+  appName = desc::desc_get_field("Package"),
+  appTitle = desc::desc_get_field("Package"),
+  appFiles = c(
+    # Add any additional files unique to your app here.
+    "R/",
+    "inst/",
+    "data-raw/",
+    "NAMESPACE",
+    "DESCRIPTION",
+    "app.R"
+  ),
+  appId = rsconnect::deployments(".")$appID,
+  lint = FALSE,
+  forceUpdate = TRUE
+)
