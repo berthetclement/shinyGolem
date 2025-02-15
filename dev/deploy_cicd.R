7811652a3cef4b0d5d82ce1e919fb2bfa3c3f3c3
clement <clement.berthet013@gmail.com>
2025-02-16 00:04:12 +0100

    deploy add setAccountInfo() to script to run to github actions

--- dev/deploy_cicd.R
+++ dev/deploy_cicd.R
@@ -1,8 +1,17 @@
 # Deploy to Posit Connect or ShinyApps.io
+
+# specific github actions
+if (!interactive()) {
+  rsconnect::setAccountInfo(
+    Sys.getenv("RSCONNECT_USER"),
+    Sys.getenv("RSCONNECT_TOKEN"),
+    Sys.getenv("RSCONNECT_SECRET")
+  )
+}
+
 # In command line.
 rsconnect::deployApp(
   appName = desc::desc_get_field("Package"),
-  account = "devappcbe",
   appTitle = desc::desc_get_field("Package"),
   appFiles = c(
     # Add any additional files unique to your app here.
